a2794cff6b922e56cc7a90d7cd060564ea691b07
bennydanangk <bennydanangkurniawan@gmail.com>
2023-12-17 20:35:08 +0700

    Commit

---
+++ application/modules/faktur/controllers/faktur.php
@@ -0,0 +1,284 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class faktur extends MY_Controller {
+
+	public function __construct()
+    {
+        parent::__construct();
+		$this->load->library('enc'); 
+        $this->load->model("M_faktur"); //load model m user
+
+		if($this->session->userdata('status') != "login"){
+			redirect(base_url("auth"));
+		}
+    }
+
+
+	public function index()
+	{
+		//set .inv
+		$id_menu = 1;
+		$data['nama_menu'] = 'faktur';
+		$this->cek_hak_akses($id_menu);
+		$set = $this->M_faktur->config();
+		$data['nama_aplikasi'] = $set[0]->nama_vendor;
+		$data['token'] = $this->session->userdata('id_hak_akses');
+		$data['nama_user'] = $this->session->userdata('nama_user');
+		$data['menu'] = $this->get_menu($this->session->userdata('id_hak_akses'));
+		
+		$this->load->view('backend/a_header',$data);
+		$this->load->view('backend/b_main',$data);
+		$this->load->view('backend/c_footer',$data);
+		
+	}
+
+
+//Menu Function
+	public function get_menu($id_hak_akses)
+	{
+				
+		$id_hak_akses =$id_hak_akses;
+		$id_hak_akses = $this->enc->out($id_hak_akses);
+			$where_parent = array(
+			'id_hak_akses' => $id_hak_akses,
+			'status_role' => 'aktif',
+			'state_orgin' => 'parent'
+
+		);
+		$menu_parent = $this->M_faktur->get_parent('role_tabel',$where_parent)->result();
+		$menu = '';
+
+		foreach ($menu_parent as $k) {
+		$menu .= '<li>';
+		$menu .='<a href="#"><i class="'.$k->icon.'"></i> <span>'.$k->nama_menu.'</span></a>';
+		$menu .='<ul>';
+
+	
+		$where_child = array(
+			'id_hak_akses' => $id_hak_akses,
+			'status_role' => 'aktif',
+			'state_orgin' => 'child',
+			'id_parent' => $k->id_menu
+
+		);
+			$menu_child = $this->M_faktur->get_parent('role_tabel',$where_child)->result();
+			foreach ($menu_child as $y) {
+				$menu .=	'<li><a href="'.base_url($y->link).'">'.$y->nama_menu.'</a></li>';
+			}
+
+			$menu .='</ul>';
+			$menu .='</li>';
+				
+			}
+
+			return $menu;
+
+					
+	}
+//end Menu 
+
+//hak akses
+function cek_hak_akses($id_menu)  {
+	$id_menu =$id_menu;
+
+	$id_hak_akses = $this->enc->out($this->session->userdata('id_hak_akses'));
+		$where = array(
+		'id_hak_akses' => $id_hak_akses,
+		'status_role' => 'aktif',
+		'role_tabel.id_menu' => $id_menu
+
+	);
+
+	$menu_cek = $this->M_faktur->get_parent('role_tabel',$where)->num_rows();
+
+	if($menu_cek > 0 ){
+
+	}else{
+		redirect(base_url('auth/logout'));
+	}
+	
+}
+//end hak akses
+
+
+function tabel_content($tgl_awal, $tgl_akhir,){
+
+	$where = array(
+		'faktur.state' => 'aktif',
+		'tgl_input 	>= ' => $tgl_awal.' 00:00:00',
+		'tgl_input <= ' => $tgl_akhir.' 23:59:59'
+	);
+
+	$data['data'] = $this->M_faktur->ambil_faktur('faktur',$where)->result();
+	$this->load->view('backend/page/tabel',$data);
+	
+}
+
+function tabel_sampah(){
+
+	$where = array(
+		'faktur.state' => 'tidak'
+	);
+
+	$data['data'] = $this->M_faktur->ambil_faktur('faktur',$where)->result();
+
+	$this->load->view('backend/page/tabel_sampah',$data);
+	
+}
+
+
+//===========ADD 
+function form_add() {
+	$data['nama_menu'] = 'data barang';
+	$where = array(
+		'state'=> 'aktif'
+	);
+
+	$data['distributor'] = $this->M_faktur->cek_where('t_distributor',$where)->result();
+	// $data['satuan'] = $this->M_faktur->cek_where('t_satuan',$where)->result();
+	// $data['kategori'] = $this->M_faktur->cek_where('t_kategori',$where)->result();
+
+
+	$this->load->view('backend/page/from_add',$data);
+}
+
+function p_add() {
+
+	$data = array(
+		'nomor_faktur' => $_POST['nomor_faktur'],
+		'id_distributor' => $_POST['id_distributor'],
+		'tgl_faktur' => $_POST['tgl_faktur'],
+		'catatan' => $_POST['catatan'],
+		'state' => 'aktif',
+		'id_user_input' => $this->session->userdata('id_user'),
+		'tgl_input'=> date('Y-m-d H:i:s'),	
+	);
+
+	$this->M_faktur->insert('faktur',$data);
+}
+
+//============= END ADD==
+
+//============EDIT
+function form_edit($id) {
+	$id= $id;
+	$data['nama_menu'] = 'Edit Data Barang';
+
+	$where_ = array(
+		'state'=> 'aktif'
+	);
+
+	$data['distributor'] = $this->M_faktur->cek_where('t_distributor',$where_)->result();
+
+
+	
+	$where = array(
+		'faktur.state'=> 'aktif',
+		'id_faktur' => $id
+	);
+
+	$data['data'] = $this->M_faktur->ambil_faktur('faktur',$where	)->result();
+
+	// print_r($data);
+	$this->load->view('backend/page/from_edit',$data);
+}
+
+function p_edit() {
+	$id = $_POST['id'];
+
+	$data = array(
+		'nomor_faktur' => $_POST['nomor_faktur'],
+		'id_distributor' => $_POST['id_distributor'],
+		'tgl_faktur' => $_POST['tgl_faktur'],
+		'catatan' => $_POST['catatan'],
+		'state' => 'aktif',
+		'id_user_input' => $this->session->userdata('id_user'),
+		'tgl_input'=> date('Y-m-d H:i:s'),	
+	);
+
+
+	$where = array(
+		'id_faktur' => $id
+	);
+
+	$this->M_faktur->update_data($where,$data,'faktur');
+}
+
+
+//=====================END EDIT
+//================== Delete
+function p_delete() {
+	$id = $_POST['id'];
+	$data = array(
+		'state' => 'tidak',
+		
+	);
+	$where = array(
+		'id_faktur' => $id
+	);
+
+	$this->M_faktur->update_data($where,$data,'faktur');
+}
+
+//=====================END Delete
+
+//================== Delete
+function p_restore() {
+	$id = $_POST['id'];
+	$data = array(
+		'state' => 'aktif',
+		
+	);
+	$where = array(
+		'id_faktur' => $id
+	);
+
+	$this->M_faktur->update_data($where,$data,'faktur');
+}
+//=====================END Delete
+
+
+//================== Delete Permanent
+function p_delete_permanen() {
+	$id = $_POST['id'];
+	
+	$where = array(
+		'id_faktur' => $id,
+		'state' => 'tidak'
+	);
+
+	$this->M_faktur->delete_data('faktur',$where);
+}
+
+//=====================END Delete Permanen
+
+
+function kode_barang()  {
+
+	$kode_ = 'KD';
+	$kode_barang = $this->M_faktur->CreateCode($kode_);
+    echo $kode_barang; 
+}
+
+function get_detail($id)  {
+
+	$id= $id;
+	$data['nama_menu'] = 'Detail Data Barang';
+
+
+	$where = array(
+		'faktur.state'=> 'aktif',
+		'id_faktur' => $id
+	);
+
+	$data['data'] = $this->M_faktur->ambil_barang('faktur',$where	)->result();
+
+	// print_r($data);
+	$this->load->view('backend/page/detail',$data);
+
+	
+}
+
+	
+}
---
+++ application/modules/faktur/views/backend/c_footer.php
@@ -0,0 +1,203 @@
+
+</html>
+
+<script>
+
+select2add();
+ function select2add() {
+  $('.select2').select2();
+    $('.select2bs4').select2({
+          theme: 'bootstrap4'
+        })
+    $("#mutasi_barang").select2({
+    dropdownParent: $("#modal_add")
+  });
+ }
+
+ 
+
+  //  $('#js-example-basic-single').select2();
+  
+  //.inv module
+var url = '<?= base_url()?>';
+var app= 'faktur';
+//end
+
+connecting(url+'api/cek_koneksi');
+
+   
+
+
+tabel_content();
+// function tabel_content() {
+//     $('#tabel_content').load(url+'/'+app+'/tabel_content');
+// }
+
+//========Open SAmpah
+
+function open_sampah() {
+    $('#tabel_content').load(url+'/'+app+'/tabel_sampah');
+}
+
+
+//=========add==
+function open_add() {
+  $('#js-example-basic-single').select2();
+    $('#tabel_content').load(url+'/'+app+'/form_add');
+    
+}
+//======= Edit===
+
+function open_edit(id) {
+    $('#tabel_content').load(url+'/'+app+'/form_edit/'+id);
+    
+}
+
+
+function open_detail(id) {
+    $('#tabel_content').load(url+'/'+app+'/get_detail/'+id);
+    
+}
+
+
+//======Del Data
+
+function hapus_data(id) {
+    
+    Swal.fire({
+  title: "Are you sure?",
+  text: "Apakah Anda Akan Menghapus Data Anda",
+  icon: "warning",
+  showCancelButton: true,
+  confirmButtonColor: "#3085d6",
+  cancelButtonColor: "#d33",
+  confirmButtonText: "Yes, delete it!"
+}).then((result) => {
+  if (result.isConfirmed) {
+
+
+	$.ajax({
+          url: url+"/"+app+"/p_delete",
+             type: 'POST',
+             data: {id:id} ,             
+             success: function(data) {    
+			
+                tabel_content();
+             }
+         });
+
+
+
+    Swal.fire({
+      title: "Deleted!",
+      text: "Your file has been deleted.",
+      icon: "success"
+    });
+  }
+});
+
+}
+
+
+
+//============Restore ==
+
+function restore_data(id) {
+    
+    Swal.fire({
+  title: "Are you sure?",
+  text: "Apakah Anda Mengembalikan Data Anda",
+  icon: "info",
+  showCancelButton: true,
+  confirmButtonColor: "#3085d6",
+  cancelButtonColor: "#d33",
+  confirmButtonText: "Yes, Restore it!"
+}).then((result) => {
+  if (result.isConfirmed) {
+
+
+	$.ajax({
+          url: url+"/"+app+"/p_restore",
+             type: 'POST',
+             data: {id:id} ,             
+             success: function(data) {    
+			
+                open_sampah();
+             }
+         });
+
+
+
+    Swal.fire({
+      title: "Restore!",
+      text: "Your file has been Restore.",
+      icon: "success"
+    });
+  }
+});
+
+}
+
+
+
+/// //============Restore ==
+
+function permanent_hapus_data(id) {
+    
+    Swal.fire({
+  title: "Are you sure?",
+  text: "Data Tidak Bisa di kembalikan Lagi",
+  icon: "warning",
+  showCancelButton: true,
+  confirmButtonColor: "#3085d6",
+  cancelButtonColor: "#d33",
+  confirmButtonText: "Yes, Terhapus Permanen!"
+}).then((result) => {
+  if (result.isConfirmed) {
+
+
+	$.ajax({
+          url: url+"/"+app+"/p_delete_permanen",
+             type: 'POST',
+             data: {id:id} ,             
+             success: function(data) {    
+			
+                open_sampah();
+             }
+         });
+
+
+
+    Swal.fire({
+      title: "Restore!",
+      text: "Your file has been Restore.",
+      icon: "success"
+    });
+  }
+});
+
+}
+
+
+
+
+function tabel_content() {
+  var tgl_awal = $('#tgl_awal').val();
+  var tgl_akhir = $('#tgl_akhir').val();
+  if(tgl_awal == '' && tgl_akhir ==''){
+tgl_awal = '<?= date("Y-m-d")?>';
+tgl_akhir = '<?= date("Y-m-d")?>';
+  }else{
+    tgl_awal = tgl_awal;
+tgl_akhir = tgl_akhir;
+
+  }
+  console.log(tgl_awal);
+  console.log(tgl_akhir);
+  $('#tabel_content').load(url+'/'+app+'/tabel_content/'+tgl_awal+'/'+tgl_akhir);
+
+}
+
+
+
+</script>
